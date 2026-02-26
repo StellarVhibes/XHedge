745af667e7cab506ce676796447ef7575fc31ecf
bbkenny <jouleself@gmail.com>
2026-02-26 03:04:27 +0100

    chore: ignore target directory and remove cached target files

--- smartcontract/target/debug/deps/volatility_shield.d
+++
@@ -1,8 +0,0 @@
-/home/babalola/Desktop/bbkenny/stellar-drips/drips/XHedge/smartcontract/target/debug/deps/volatility_shield.d: contracts/volatility_shield/src/lib.rs contracts/volatility_shield/src/test.rs
-
-/home/babalola/Desktop/bbkenny/stellar-drips/drips/XHedge/smartcontract/target/debug/deps/libvolatility_shield.so: contracts/volatility_shield/src/lib.rs contracts/volatility_shield/src/test.rs
-
-/home/babalola/Desktop/bbkenny/stellar-drips/drips/XHedge/smartcontract/target/debug/deps/libvolatility_shield.rlib: contracts/volatility_shield/src/lib.rs contracts/volatility_shield/src/test.rs
-
-contracts/volatility_shield/src/lib.rs:
-contracts/volatility_shield/src/test.rs:
